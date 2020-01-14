720d14eaf3cad205842468fb3153a17dd580d757
saito <kssaitoukanta@gmail.com>
2020-01-14 12:50:22 +0000

    first commit

---
+++ 03-kadai1.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Saito Kanta";
+if ($name == "Saito Kanta" ) {
+  echo "私は".$name."です。";
+  }else{
+  echo $name."ではありません。";
+  }
---
+++ 03-kadai3.php
@@ -0,0 +1,6 @@
+<?php
+$fruits = array("orange", "melon", "apple", "cherry", "banana");
+foreach($fruits as $fruit){
+  echo $fruit;
+  echo "\n";
+}
